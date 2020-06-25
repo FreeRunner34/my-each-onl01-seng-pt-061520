9b7ff39848d55f99cf88eadddb2c02888d2fcf9d
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-25 04:55:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,11 @@
-collection = [1,2,3,4]
-
-def my_each (collection )
-  collection 
+def reverse_each_word(sentence)
+2.3.1 :040 >   new_sentence = []
+2.3.1 :041?>   sentence.split.each do |backward_words|
+2.3.1 :042 >     new_sentence << backward_words.reverse
+2.3.1 :043?>   end
+2.3.1 :044?>   new_sentence.join(" ")
+2.3.1 :045?> end
+ => :reverse_each_word
+2.3.1 :046 > reverse_each_word("Hello there, and how are you?")
+ => "olleH ,ereht dna woh era ?uoy"
+2.3.1 :047 > exit
